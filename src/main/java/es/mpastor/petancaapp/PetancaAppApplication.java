0f0a2a785283f1b4e7cfa9dd99503cbfce49e978
Mauro Pastor Anduix <mauropasan01@gmail.com>
2022-06-11 16:29:12 +0200

    Initial commit

---
+++ src/main/java/es/mpastor/petancaapp/PetancaAppApplication.java
@@ -0,0 +1,13 @@
+package es.mpastor.petancaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetancaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetancaAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/mpastor/petancaapp/PetancaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package es.mpastor.petancaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetancaAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
